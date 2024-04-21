8c4673d8c6ca316ea36c5c5464c2f165ce85245e
reservoirinvest <reservoirinvest@gmail.com>
2024-04-21 17:58:14 +0800

    Update util.py
    Automatic getting of current year holidays from nse website added.

--- nse_fno_expiry_calculator/util.py
+++ nse_fno_expiry_calculator/util.py
@@ -1,22 +1,66 @@
-import pendulum
-from pendulum.date import Date
-import logging
+import json
 
-listOfNseHolidays = set([
-    Date(2022, 1, 26),  # Republic Day
-    Date(2022, 3, 1),  # Maha Shivaratri
-    Date(2022, 3, 18),  # Holi
-    Date(2022, 4, 14),  # Dr.Baba Saheb Ambedkar Jayanti
-    Date(2022, 4, 15),  # Good friday
-    Date(2022, 5, 3),  # Id-ul-Fitr
-    Date(2022, 8, 9),  # Moharram
-    Date(2022, 8, 15),  # Independence Day
-    Date(2022, 8, 31),  # Ganesh Chaturthi
-    Date(2022, 10, 5),  # Vijaya Dashami
-    Date(2022, 10, 24),  # Diwali-Laxmi Pujan
-    Date(2022, 10, 26),  # Diwali-Balipratipada
-    Date(2022, 11, 8)   # Guru Nanak Jayanti
-])
+import pendulum
+import requests
+from pendulum.date import Date
+import pandas as pd
+
+headers = {
+    'User-agent': 'Mozilla/5.0',
+    'Accept': '/', 
+    'Connection': 'keep-alive', 
+    "Referer": "https://www.nseindia.com/resources/exchange-communication-holidays"
+    }
+
+def get_nse_payload(url: str) -> requests.models.Response:
+
+    """
+    Returns response
+
+    url: (samples)
+    * "https://www.nseindia.com/api/holiday-master?type=trading" ... for nse trading holidays
+
+    Response processing examples:
+    ---
+    `df = pd.read_csv(io.StringIO(response.text))` for csv type response output 
+
+    """
+
+    base_url = 'https://www.nseindia.com'
+    session = requests.Session()
+
+    response=requests.get(url, headers=headers)
+
+    return response
+
+
+def get_nse_holidays() -> set:
+
+    """
+    Gets holidays from nse website
+    """
+
+    url = "https://www.nseindia.com/api/holiday-master?type=trading"
+
+    r = get_nse_payload(url)
+
+    holiday_dict = json.loads(r.text).get('CBM')
+    df_nse_holidays = pd.DataFrame.from_records(holiday_dict)
+
+    out = set(pd.to_datetime(df_nse_holidays.tradingDate).dt.date.apply(pendulum.instance))
+
+    return out
+    
+
+def __considerHolidayList(expiryDate: Date):
+
+    listOfNseHolidays = get_nse_holidays()
+
+    if(expiryDate.date() in listOfNseHolidays):
+        return __considerHolidayList(expiryDate.subtract(days=1))
+    else:
+        return expiryDate
+
 
 def getNearestWeeklyExpiryDate():
     expiryDate = None
@@ -51,15 +95,8 @@ def getNextMonthlyExpiryDate():
         expiryDate = pendulum.now().add(months=1).last_of('month', pendulum.THURSDAY)
     return __considerHolidayList(expiryDate).date()
 
-
-# utility method to be used only by this module
-def __considerHolidayList(expiryDate: Date):
-    if(expiryDate.date() in listOfNseHolidays):
-        return __considerHolidayList(expiryDate.subtract(days=1))
-    else:
-        return expiryDate
-
-
+if __name__ == "__main__":
+    print(getNextMonthlyExpiryDate())
 # print('today is '+str(pendulum.now().date()))
 # print('nearest weekly exp is '+str(getNearestWeeklyExpiryDate()))
 # print('next weekly exp is '+str(getNextWeeklyExpiryDate()))
